980c997373f18a7ed593e5101ab671d6f588ef93
Diego Velasquez <diesveca@hotmail.com>
2020-07-24 09:48:09 -0700

    Added home page not finished

---
+++ frontend/src/containers/ClientHome.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Container from '@material-ui/core/Container'
+
+const ClientHome = () => {
+	return (
+			<>
+				<Container>
+					<h1>Bienvenido a nuestra pagina</h1>
+					<p>Tenemos la mas amplia variedad en muebles de la mas alta calidad.</p>
+				</Container>
+			</>
+	);
+}
+
+export default ClientHome;
