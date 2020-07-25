5d8f66dd055600c802a18fac87e2376e7a0dfd61
Diego Velasquez <diesveca@hotmail.com>
2020-07-25 16:34:00 -0700

    Added axios

--- frontend/src/components/ProductList/ProductList.js
+++ frontend/src/components/ProductList/ProductList.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import './ProductList.css'
 import Product from '../Product/Product'
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container'
 import { makeStyles } from '@material-ui/core/styles';
 import { Box } from '@material-ui/core';
+import axios from 'axios'
 
 const useStyles = makeStyles({
   productContainer: {
@@ -19,6 +20,11 @@ const useStyles = makeStyles({
 const ProductList = () => {
   const classes = useStyles();
   const productDescription = {title: "Mueble", description: "Mueble de madera", price: 60000, available_quantity: 5}
+  useEffect(() => {
+    axios.get('/product/furniture/').then((forniture) => {
+      productList = forniture.data
+    })
+  })
   const productList = [] 
   for (let index = 0; index < 10; index++) {
     productList.push(productDescription)
