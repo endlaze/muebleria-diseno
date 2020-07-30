ee9488738386468b3c60586bae7319effdb0ae6e
Endlaze <asanchezg811@gmail.com>
2020-07-29 20:27:33 -0600

    feat: added combos

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Material, FurnitureType, Furniture
+from .models import Material, FurnitureType, Furniture, FurnitureComboItem, FurnitureCombo
 from locations.models import Workplace
 from locations.serializers import WorkplaceSerializer
 
@@ -70,3 +70,64 @@ class FurnitureSerializer(ProductSerializer):
         furniture.materials.add(*materials_ids)
         furniture.save()
         return furniture
+
+
+class FurnitureComboItemSerializer(serializers.ModelSerializer):
+    materials = MaterialSerializer(read_only=True, many=True)
+    materials_ids = serializers.PrimaryKeyRelatedField(
+        queryset=Material.objects.all(),
+        write_only=True,
+        many=True
+    )
+
+    furn_type = FurnitureTypeSerializer(read_only=True)
+    furn_type_id = serializers.PrimaryKeyRelatedField(
+        queryset=FurnitureType.objects.all(),
+        write_only=True
+    )
+
+    class Meta:
+        model = FurnitureComboItem
+        fields = ['quantity', 'materials', 'materials_ids', 'furn_type',
+                  'furn_type_id']
+
+
+class FurnitureComboSerializer(ProductSerializer):
+    id = serializers.ReadOnlyField()
+    combo_products = FurnitureComboItemSerializer(many=True)
+    workshop = WorkplaceSerializer(read_only=True)
+
+    workshop_id = serializers.PrimaryKeyRelatedField(
+        queryset=Workplace.objects.all(),
+        write_only=True
+    )
+
+    class Meta:
+        model = FurnitureCombo
+        fields = ProductSerializer.Meta.fields + \
+            ['combo_products', 'workshop', 'workshop_id']
+
+    def create(self, validated_data):
+        combo_products = validated_data.pop('combo_products')
+
+        furniture_combo = FurnitureCombo.objects.create(
+            workshop=validated_data.pop('workshop_id'),
+            **validated_data
+        )
+
+        for prod in combo_products:
+            self.createComboItem(furniture_combo, **prod)
+
+        return furniture_combo
+
+    def createComboItem(self, furn_combo, materials_ids, furn_type_id, quantity):
+        new_combo_item = FurnitureComboItem.objects.create(
+            furn_combo=furn_combo,
+            furn_type=furn_type_id,
+            quantity=quantity
+        )
+
+        new_combo_item.materials.add(*materials_ids)
+        new_combo_item.save()
+
+        return new_combo_item
--- backend/products/urls.py
+++ backend/products/urls.py
@@ -1,8 +1,9 @@
-from .views import MaterialViewSet, FurnitureTypeViewSet, FurnitureViewSet
+from .views import MaterialViewSet, FurnitureTypeViewSet, FurnitureViewSet, FurnitureComboViewSet
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register('material', MaterialViewSet, 'material')
 router.register('furniture_type', FurnitureTypeViewSet, 'furniture_type')
 router.register('furniture', FurnitureViewSet, 'furniture')
+router.register('furniture_combo', FurnitureComboViewSet, 'furniture_combo')
 urlpatterns = router.urls
